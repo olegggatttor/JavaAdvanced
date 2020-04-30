f60f0ef97657f747c7d46a645204201b0dffc9b8
Oleg Bobrov <njancrafttv@gmail.com>
2020-04-30 13:48:45 +0300

    upd udp

---
+++ java-solutions/ru/ifmo/rain/bobrov/hello/HelloUDPClient.java
@@ -0,0 +1,110 @@
+package ru.ifmo.rain.bobrov.hello;
+
+import info.kgeorgiy.java.advanced.hello.HelloClient;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Public class provides client interface working with UDP protocol and implementing {@link HelloClient}
+ */
+public class HelloUDPClient implements HelloClient {
+
+
+    /**
+     * Default constructor.
+     */
+    public HelloUDPClient() {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run(final String host, final int port, final String prefix, final int threads, final int requests) {
+        final ExecutorService requestService = Executors.newFixedThreadPool(threads);
+        final InetAddress serverHost;
+        try {
+            serverHost = InetAddress.getByName(host);
+        } catch (UnknownHostException ex) {
+            System.err.println("Unknown host name:" + host);
+            return;
+        }
+        for (int i = 0; i < threads; i++) {
+            requestService.submit(() -> sendRequest(port, prefix, requests, serverHost, threads));
+        }
+        requestService.shutdown();
+        try {
+            if (!requestService.awaitTermination(10000L, TimeUnit.SECONDS)) {
+                requestService.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            requestService.shutdownNow();
+        }
+    }
+
+    private void sendRequest(final int port, final String prefix, final int requests, final InetAddress serverHost,
+                             final int threads) {
+        DatagramSocket socket;
+        final int responseSize;
+        try {
+            socket = new DatagramSocket();
+            socket.setSoTimeout(1000);
+            responseSize = socket.getReceiveBufferSize();
+        } catch (SocketException ex) {
+            return;
+        }
+        final long n = Thread.currentThread().getId() % threads;
+        for (int i = 0; i < requests; i++) {
+            final String requestName = (prefix + n + "_" + i);
+            final byte[] request = requestName.getBytes(StandardCharsets.UTF_8);
+            final DatagramPacket packet = new DatagramPacket(request, request.length, serverHost, port);
+            while (!socket.isClosed()) {
+                try {
+                    socket.send(packet);
+                    final byte[] response = new byte[responseSize];
+                    final DatagramPacket packetReceive = new DatagramPacket(response, response.length);
+                    socket.receive(packetReceive);
+                    final String answer = new String(packetReceive.getData(), packetReceive.getOffset(), packetReceive.getLength(),
+                            StandardCharsets.UTF_8);
+                    if (answer.equals("Hello, " + requestName)) {
+                        System.out.println(answer);
+                        break;
+                    }
+                } catch (IOException ignored) {
+
+                }
+            }
+        }
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length != 5 || Arrays.stream(args).anyMatch(Objects::isNull)) {
+            System.err.println("Wrong arguments");
+            return;
+        }
+        final HelloClient client = new HelloUDPClient();
+        try {
+            final String host = args[0];
+            final int port = Integer.parseInt(args[1]);
+            final String prefix = args[2];
+            final int threads = Integer.parseInt(args[3]);
+            final int requestsPerThread = Integer.parseInt(args[4]);
+            client.run(host, port, prefix, threads, requestsPerThread);
+        } catch (NumberFormatException ex) {
+            System.err.println("Port, thread and requests per thread must be values of type int.");
+        }
+    }
+}
---
+++ java-solutions/ru/ifmo/rain/bobrov/hello/HelloUDPServer.java
@@ -0,0 +1,98 @@
+package ru.ifmo.rain.bobrov.hello;
+
+import info.kgeorgiy.java.advanced.hello.HelloServer;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Class of server working with UDP protocol implementing {@link HelloServer}
+ */
+public class HelloUDPServer implements HelloServer {
+    private ExecutorService receiveService = null;
+    private DatagramSocket socket = null;
+
+
+    /**
+     * Default constructor.
+     */
+    public HelloUDPServer() {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void start(int port, int threads) {
+        receiveService = Executors.newFixedThreadPool(threads);
+        final int requestSize;
+        try {
+            socket = new DatagramSocket(port);
+            socket.setSoTimeout(1000);
+            requestSize = socket.getReceiveBufferSize();
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+            return;
+        }
+        for (int i = 0; i < threads; i++) {
+            final byte[] request = new byte[requestSize];
+            final DatagramPacket packet = new DatagramPacket(request, requestSize);
+            receiveService.submit(() -> {
+                while (!socket.isClosed()) {
+                    try {
+                        socket.receive(packet);
+                        byte[] answer = ("Hello, " + new String(packet.getData(), 0, packet.getLength(), StandardCharsets.UTF_8)).getBytes(StandardCharsets.UTF_8);
+                        socket.send(new DatagramPacket(answer, answer.length, packet.getSocketAddress()));
+                    } catch (IOException ignored) {
+
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() {
+        socket.close();
+        /*receiveService.shutdown();
+        try {
+            if (!receiveService.awaitTermination(1000L, TimeUnit.SECONDS)) {
+                receiveService.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            receiveService.shutdownNow();
+        }*/
+        receiveService.shutdownNow();
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        if (args.length != 2 || Arrays.stream(args).anyMatch(Objects::isNull)) {
+            System.err.println("Wrong arguments");
+            return;
+        }
+        HelloServer server = new HelloUDPServer();
+        try {
+            final int port = Integer.parseInt(args[0]);
+            final int threads = Integer.parseInt(args[1]);
+            server.start(port, threads);
+        } catch (NumberFormatException ex) {
+            System.err.println("Port and threads should be values of type int.");
+        }
+    }
+}
